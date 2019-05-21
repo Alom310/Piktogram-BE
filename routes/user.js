285b8184bb7aae51bc883f2fbc7a1c3d4d0b3f14
alom310 <alom310@gmail.com>
2019-05-21 16:10:14 -0700

    oauth

--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const controllers = require("../controllers");
+// const controllers = require("../controllers");
 
-router.put('/:id/update', controllers.user.update);
-router.delete("/delete", controllers.user.delete);
-router.get("/", controllers.user.show);
+// router.put('/:id/update', controllers.user.update);
+// router.delete("/delete", controllers.user.delete);
+// router.get("/", controllers.user.show);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const cors = require("cors");
 const app = express();
 const ig = require("instagram-node").instagram();
 
-const userRoutes = require("./routes/user.js");
-const postRoutes = require("./routes/post");
-const likeRoutes = require("./routes/like");
-const commentRoutes = require("./routes/comment");
+// const userRoutes = require("./routes/user.js");
+// const postRoutes = require("./routes/post");
+// const likeRoutes = require("./routes/like");
+// const commentRoutes = require("./routes/comment");
 
 
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -19,22 +19,33 @@ app.get('/', (req, res) => {
   res.send('test');
 })
 
-app.use("/users", userRoutes);
-app.use("/posts", postRoutes);
-app.use("/likes", likeRoutes);
-app.use("/comments", commentRoutes);
+// app.use("/users", userRoutes);
+// app.use("/posts", postRoutes);
+// app.use("/likes", likeRoutes);
+// app.use("/comments", commentRoutes);
 
 ig.use({
     client_id: "9acb7ddc5f4d47178844c1faf0907acd",
     client_secret: "f77a4a4fd3c141128fcf3ded6310610d"
 });
 
-const redirectUri = "https://localhost:3000/redirect";
+const redirectUri = "https://localhost:3001/redirect";
 
 app.get('/authorize', function(req, res){
     // set the scope of our application to be able to access likes and public content
     res.redirect(ig.get_authorization_url(redirectUri, { scope : ['public_content','likes']}) );
 });
+
+app.get('/register', function(req, res){
+    //retrieves the code that was passed along as a query to the '/handleAuth' route and uses this code to construct an access token
+    ig.authorize_user(req.query.code, redirectUri, function(err, result){
+        if(err) res.send( err );
+    // store this access_token in a global variable called accessToken
+        accessToken = result.access_token;
+    // After getting the access_token redirect to the '/' route 
+        res.redirect('/');
+    });
+})
 
 app.listen(process.env.PORT || 3001, () =>
   console.log("Server is now running")
