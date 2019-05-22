3c9c2ab4de132548999d3e1bb548b1ccd9117432
josephlbaker <joelucasbaker@gmail.com>
2019-05-21 17:19:03 -0700

    login working

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(express.static(__dirname + "/public"));
 app.use(cors());
 
 app.get('/', (req, res) => {
-  res.send('test');
+    res.send('test');
 })
 
 // app.use("/users", userRoutes);
@@ -29,24 +29,32 @@ ig.use({
     client_secret: "f77a4a4fd3c141128fcf3ded6310610d"
 });
 
-const redirectUri = "https://localhost:3001/redirect";
+const redirectUri = "http://localhost:3000/redirect/";
 
-app.get('/authorize', function(req, res){
+app.get('/authorize', function (req, res) {
     // set the scope of our application to be able to access likes and public content
-    res.redirect(ig.get_authorization_url(redirectUri, { scope : ['public_content','likes']}) );
+    res.redirect(ig.get_authorization_url(redirectUri, { scope: ['public_content', 'likes'] }));
 });
 
-app.get('/register', function(req, res){
+app.get('/redirect/', function (req, res) {
     //retrieves the code that was passed along as a query to the '/handleAuth' route and uses this code to construct an access token
-    ig.authorize_user(req.query.code, redirectUri, function(err, result){
-        if(err) res.send( err );
-    // store this access_token in a global variable called accessToken
+    ig.authorize_user(req.query.code, redirectUri, function (err, result) {
+        if (err) res.send(err);
+        // store this access_token in a global variable called accessToken
         accessToken = result.access_token;
-    // After getting the access_token redirect to the '/' route 
+        // After getting the access_token redirect to the '/' route
         res.redirect('/');
     });
 })
 
+// app.get('/', function (req, res) {
+//     // create a new instance of the use method which contains the access token gotten
+//     ig.use({
+//         access_token: accessToken
+//     });
+// });
+
+
 app.listen(process.env.PORT || 3001, () =>
-  console.log("Server is now running")
+    console.log("Server is now running")
 );
