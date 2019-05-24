8cf063836ce6571335ebae35b544e337f4878b93
josephlbaker <joelucasbaker@gmail.com>
2019-05-24 15:48:03 -0700

    display images on FE

--- controllers/resources.js
+++ controllers/resources.js
@@ -1,6 +1,28 @@
+const path = require('path');
+
 module.exports = {
   images: (req, res) => {
-    let filePath = "./uploads/" + req.params.name;
-    res.download(filePath);
+    // let filePath = "./uploads/" + req.params.name;
+    let mimeType = {
+      "jpg":"image/jpg",
+      "JPG":"image/jpg",
+      "png":"image/png",
+      "PNG":"image/png",
+      "jpeg":"image/jpeg",
+      "JPEG":"image/jpeg",
+      "gif":"image/gif",
+      "GIF":"image/GIF"
+    }
+    let fileName = req.params.name;
+    let ext= fileName.substr(fileName.lastIndexOf('.')+1);
+    let filepath = path.resolve(__dirname + '/../uploads/') +'/'+ fileName;
+    console.log(filepath)
+    res.type(mimeType[ext]).sendFile(filepath);
+
+
+    // res.sendFile('index.html', { root: __dirname });
+
+
+    // res.download(filePath);
   }
 }
